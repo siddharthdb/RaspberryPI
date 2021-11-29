0bfeaaea52642fa40be492b2080451dd4eb8f65a
Siddharth Bhawalkar <siddharthdb@gmail.com>
2021-11-29 12:42:35 -0500

    custom driver I2C

--- stock-ticker.py
+++ stock-ticker.py
@@ -1,30 +1,11 @@
-from RPLCD.i2c import CharLCD
+import I2C_LCD_driver
 import os
 from dotenv import load_dotenv
 import json
 import time
 import http.client
-import urllib.parse
-
 
 load_dotenv()
-
-
-def write_to_lcd(lcd, framebuffer, num_cols):
-    lcd.home()
-    for row in framebuffer:
-        lcd.write_string(row.ljust(num_cols)[:num_cols])
-        lcd.write_string('\r\n')
-
-
-def loop_string(string, lcd, framebuffer, row, num_cols, delay=0.15):
-    padding = ' ' * num_cols
-    s = padding + string + padding
-    for i in range(len(s) - num_cols + 1):
-        framebuffer[row] = s[i:i+num_cols]
-        write_to_lcd(lcd, framebuffer, num_cols)
-        time.sleep(delay)
-
 
 def format_num(num):
     return "{:.2f}".format(num)
@@ -39,11 +20,11 @@ headers = {
     'x-rapidapi-key': os.getenv("RAPID_API")
 }
 
-stocks = ["AAPL","ADBE","ADI","ADP","ADSK","AEP","ALGN","AMAT","AMD","AMGN","AMZN","ANSS","ASML","ATVI","AVGO","BIDU","BIIB","BKNG","CDNS","CDW","CERN","CHKP",
-"CHTR","CMCSA","COST","CPRT","CRWD","CSCO","CSX","CTAS","CTSH","DLTR","DOCU","DXCM","EA","EBAY","EXC","FAST","FB","FISV","FOX",
-"FOXA","GILD","GOOG","GOOGL","HON","IDXX","ILMN","INCY","INTC","INTU","ISRG","JD","KDP","KHC","KLAC","LRCX","LULU","MAR","MCHP",
-"MDLZ","MELI","MNST","MRNA","MRVL","MSFT","MTCH","MU","NFLX","NTES","NVDA","NXPI","OKTA","ORLY","PAYX","PCAR","PDD","PEP","PTON","PYPL","QCOM","REGN","ROST","SBUX",
-"SGEN","SIRI","SNPS","SPLK","SWKS","TCOM","TEAM","TMUS","TSLA","TXN","VRSK","VRSN","VRTX","WBA","WDAY","XEL","XLNX","ZM"]
+stocks = ["AAPL", "ADBE", "ADI", "ADP", "ADSK", "AEP", "ALGN", "AMAT", "AMD", "AMGN", "AMZN", "ANSS", "ASML", "ATVI", "AVGO", "BIDU", "BIIB", "BKNG", "CDNS", "CDW",
+          "CERN", "CHKP", "CHTR", "CMCSA", "COST", "CPRT", "CRWD", "CSCO", "CSX", "CTAS", "CTSH", "DLTR", "DOCU", "DXCM", "EA", "EBAY", "EXC", "FAST", "FB", "FISV", 
+          "FOX", "FOXA", "GILD", "GOOG", "GOOGL", "HON", "IDXX", "ILMN", "INCY", "INTC", "INTU", "ISRG", "JD", "KDP", "KHC", "KLAC", "LRCX", "LULU", "MAR", "MCHP",
+          "MDLZ", "MELI", "MNST", "MRNA", "MRVL", "MSFT", "MTCH", "MU", "NFLX", "NTES", "NVDA", "NXPI", "OKTA", "ORLY", "PAYX", "PCAR", "PDD", "PEP", "PTON", "PYPL",
+          "QCOM", "REGN", "ROST", "SBUX", "SGEN", "SIRI", "SNPS", "SPLK", "SWKS", "TCOM", "TEAM", "TMUS", "TSLA", "TXN", "VRSK", "VRSN", "VRTX", "WBA", "WDAY", "XEL", "XLNX", "ZM"]
 
 conn.request("GET", "/market/get-realtime-prices?symbols=" + "%2C".join(stocks),
              headers=headers)
@@ -56,15 +37,17 @@ ticker_data = json.loads(data.decode("utf-8"))["data"]
 stock_data = ""
 for ticker_obj in ticker_data:
     stock_attributes = ticker_obj["attributes"]
-    stock_data = stock_data + stock_attributes["identifier"] + ": " + format_num(stock_attributes["last"]) + "(" + format_num(stock_attributes["change"]) + ") "
+    stock_data = stock_data + stock_attributes["identifier"] + ": " + format_num(
+        stock_attributes["last"]) + "(" + format_num(stock_attributes["change"]) + ") "
 
-framebuffer = [
-    'Stock Ticker',
-    '',
-]
-
-lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=16, rows=2,
-              dotsize=8, charmap='A02', auto_linebreaks=True, backlight_enabled=True)
+lcd = I2C_LCD_driver.lcd()
+str_pad = " " * 16
+stock_data = str_pad + stock_data
 
 while True:
-    loop_string(stock_data, lcd, framebuffer, 1, 16)
+    for i in range(0, len(stock_data)):
+        lcd_text = stock_data[i:(i+16)]
+        lcd.lcd_display_string("Stock Ticker!", 1)
+        lcd.lcd_display_string(lcd_text, 2)
+        time.sleep(0.08)
+        lcd.lcd_display_string(str_pad, 2)
